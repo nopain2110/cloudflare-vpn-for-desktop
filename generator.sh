651362ce85465724b655865e2c20da81f0212019
Tuyen Tran <tuyen.trung.tran@mgm-tp.com>
2020-02-03 12:08:45 +0700

    Modify generator

--- generator.sh
+++ generator.sh
@@ -7,6 +7,5 @@ mkdir -p configs; cp register.py ./configs \
 docker run --rm -i -v `pwd`/configs:/root -w /root ubuntu:devel sh -c '\
 apt update; apt install -y wireguard python3-pip; \
 wg genkey > privatekey; wg pubkey < privatekey > publickey; \
-pip3 install requests; python3 register.py' && \
-rm -rf configs/.*; \
+pip3 install requests; python3 register.py'; \
 docker run --rm -d -p 80:80 -v `pwd`/configs:/usr/local/apache2/htdocs/ httpd
