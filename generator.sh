f5e5524a9bdaedc30095eca76d560090006320cb
Tuyen Tran <tuyen.trung.tran@mgm-tp.com>
2020-02-03 12:07:31 +0700

    Copy register.py to configs folder

--- generator.sh
+++ generator.sh
@@ -3,10 +3,10 @@
 # Scripted by Tuyen Tran
 # http://fb.com/gr219
 # -------------------
-mkdir -p configs; cp register.py configs/ \
+mkdir -p configs; cp register.py ./configs \
 docker run --rm -i -v `pwd`/configs:/root -w /root ubuntu:devel sh -c '\
 apt update; apt install -y wireguard python3-pip; \
 wg genkey > privatekey; wg pubkey < privatekey > publickey; \
-pip3 install requests; python3 register.py'; \
-rm -rf .*; \
+pip3 install requests; python3 register.py' && \
+rm -rf configs/.*; \
 docker run --rm -d -p 80:80 -v `pwd`/configs:/usr/local/apache2/htdocs/ httpd
