9f029dd55dd826602ad1f8f0ef5504023a69a286
Tuyen Tran <tuyen.trung.tran@mgm-tp.com>
2020-02-03 14:19:38 +0700

    Rename sh files

--- boost.sh
+++ boost.sh
@@ -9,5 +9,5 @@ interval=$2
 
 docker run -v `pwd`/getdata.sh:/getdata.sh --rm -i alpine sh -c \
 "apk add pwgen curl; \
-chmod 777 /boost.sh; \
-sh /boost.sh $referal $interval"
+chmod 777 /getdata.sh; \
+sh /getdata.sh $referal $interval"
