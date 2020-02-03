71bcbf168ea7dda5967ebea43eb9f027ec2090ff
Tuyen Tran <tuyen.trung.tran@mgm-tp.com>
2020-02-03 14:25:18 +0700

    Rename sh files

--- generator.sh
+++ generator.sh
@@ -7,5 +7,8 @@ mkdir -p configs; cp register.py configs/; \
 docker run --rm -i -v `pwd`/configs:/root -w /root ubuntu:devel sh -c '\
 apt update; apt install -y wireguard python3-pip; \
 pip3 install requests; python3 register.py'; \
+echo "=====wgcf-profile.conf=====\n"; cat configs/wgcf-profile.conf; \
+echo "=====wgcf-identity.json=====\n"; cat configs/wgcf-identity.json; \
+echo "=============================\n"; \
 docker run --rm -d -p 80:80 -v `pwd`/configs:/usr/local/apache2/htdocs/ httpd; \
 rm configs/register.py;
