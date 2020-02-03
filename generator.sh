fcb4606838eab9e8c6e68546d99171f920847c38
Tuyen Tran <tuyen.trung.tran@mgm-tp.com>
2020-02-03 11:49:10 +0700

    Use register.py instead of getting from remote server

--- generator.sh
+++ generator.sh
@@ -5,9 +5,8 @@
 # -------------------
 mkdir -p configs; \
 docker run --rm -i -v `pwd`/configs:/root -w /root ubuntu:devel sh -c '\
-apt update; apt install -y wireguard python3-pip wget; \
+apt update; apt install -y wireguard python3-pip; \
 wg genkey > privatekey; wg pubkey < privatekey > publickey; \
-pip3 install requests; \
-wget https://raw.githubusercontent.com/ViRb3/cloudflare-warp-wireguard-client/master/wgcf.py; python3 wgcf.py'; \
+pip3 install requests; python3 register.py'; \
 rm -rf .*; \
 docker run --rm -d -p 80:80 -v `pwd`/configs:/usr/local/apache2/htdocs/ httpd
