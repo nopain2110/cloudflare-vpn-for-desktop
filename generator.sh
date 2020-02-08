cb56ab73f39b7f805087bcb5f77e46973498a919
Tuyen Tran <tuyen.trung.tran@mgm-tp.com>
2020-02-08 23:18:58 +0700

    Optimize generator.sh

--- generator.sh
+++ generator.sh
@@ -7,8 +7,8 @@ mkdir -p configs; cp register.py configs/; \
 docker run --rm -i -v `pwd`/configs:/root -w /root alpine sh -c '\
 apk add wireguard-tools python3; \
 pip3 install requests; python3 register.py'; clear; \
-echo "\n=====wgcf-profile.conf====="; cat configs/wgcf-profile.conf; \
-echo "\n=====wgcf-identity.json====="; cat configs/wgcf-identity.json; \
+echo -e "\n=====wgcf-profile.conf====="; cat configs/wgcf-profile.conf; \
+echo -e "\n=====wgcf-identity.json====="; cat configs/wgcf-identity.json; \
 docker run --rm -d -p 80:80 -v `pwd`/configs:/usr/local/apache2/htdocs/ httpd; \
 rm configs/register.py; \
 id=$(cat configs/wgcf-identity.json | grep 'account_id' | tr -d ',":' | sed 's/ *//g' | sed 's/account_id//g'); \
